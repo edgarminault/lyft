8f804aa45abfa8bef49a43a5364b4eb6ac297237
Edgar Minault <e.minault@bluewin.ch>
2020-09-29 21:35:40 +0200

    Trainer, Data, Functions, Parameters created.

---
+++ lyft/data.py
@@ -0,0 +1,46 @@
+import pandas as pd
+from google.cloud import storage
+
+from lyft.parameters import BUCKET_NAME, AWS_PATH
+
+DIST_ARGS = dict(
+    start_lat="pickup_latitude",
+    start_lon="pickup_longitude",
+    end_lat="dropoff_latitude",
+    end_lon="dropoff_longitude",
+)
+
+
+def train_data(nrows=10000, local=False, **kwargs):
+    """method to get the training data (or a portion of it) from google cloud bucket"""
+    # Add Client() here
+    path = AWS_PATH
+    df = pd.read_csv(path, nrows=nrows)
+    return df
+
+
+def clean_df(df, test=False):
+    """Based on EDA notebook findings.
+    - outliers in latitude and longitude deleted
+    - fare and passengers discrepencies deleted
+    """
+    df = df.dropna(how="any", axis="rows")
+    df = df[(df.dropoff_latitude != 0) | (df.dropoff_longitude != 0)]
+    df = df[(df.pickup_latitude != 0) | (df.pickup_longitude != 0)]
+    if "fare_amount" in list(df):
+        df = df[df.fare_amount.between(0, 4000)]
+    df = df[df.passenger_count < 8]
+    df = df[df.passenger_count >= 0]
+    df = df[df["pickup_latitude"].between(left=40, right=42)]
+    df = df[df["pickup_longitude"].between(left=-75, right=-72)]
+    df = df[df["dropoff_latitude"].between(left=40, right=42)]
+    df = df[df["dropoff_longitude"].between(left=-75, right=-72)]
+    return df
+
+
+if __name__ == "__main__":
+    params = dict(
+        nrows=1000,
+        local=False,  # set to False to get data from GCP (Storage or BigQuery)
+    )
+    df = train_data(**params)
---
+++ lyft/parameters.py
@@ -0,0 +1,36 @@
+### GCP configuration - - - - - - - - - - - - - - - - - - -
+
+# /!\ you should fill these according to your account
+
+### MLFLOW URI Link - - - - - - - - - - - - - - - - - - - -
+
+MLFLOW_URI = "https://mlflow.lewagon.co/"
+
+### GCP Project - - - - - - - - - - - - - - - - - - - - - -
+
+PROJECT_ID = "wagon-bootcamp-288408"
+
+### GCP Storage - - - - - - - - - - - - - - - - - - - - - -
+
+BUCKET_NAME = "wagon-ml-edgarminault-gcp"
+
+##### Data  - - - - - - - - - - - - - - - - - - - - - - - -
+
+TEST_DATA_PATH = "data/test.csv"
+
+##### Training  - - - - - - - - - - - - - - - - - - - - - -
+
+AWS_PATH = "s3://wagon-public-datasets/taxi-fare-train.csv"
+
+##### Model - - - - - - - - - - - - - - - - - - - - - - - -
+
+# model folder name (will contain the folders for all trained model versions)
+MODEL_NAME = "lyft"
+
+# model version folder name (where the trained model.joblib file will be stored)
+
+### GCP AI Platform - - - - - - - - - - - - - - - - - - - -
+
+# not required here
+
+### - - - - - - - - - - - - - - - - - - - - - - - - - - - -
